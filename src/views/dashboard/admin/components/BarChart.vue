1628bc2911483688bbadb3460d8083708dcec3db
carter <1105563004@qq.com>
2019-12-03 16:03:07 +0800

    本周销售额图表修改mock->后端获取

--- src/views/dashboard/admin/components/BarChart.vue
+++ src/views/dashboard/admin/components/BarChart.vue
@@ -6,93 +6,112 @@
 import echarts from 'echarts'
 require('echarts/theme/macarons') // echarts theme
 import resize from './mixins/resize'
+import { sumSalesLatestWeek } from '@/api/home'
 
-const animationDuration = 6000
+const animationDuration = 3600
 
 export default {
-  mixins: [resize],
-  props: {
-    className: {
-      type: String,
-      default: 'chart'
+    mixins: [resize],
+    props: {
+      className: {
+        type: String,
+        default: 'chart'
+      },
+      width: {
+        type: String,
+        default: '100%'
+      },
+      height: {
+        type: String,
+        default: '283px'
+      }
     },
-    width: {
-      type: String,
-      default: '100%'
+    data() {
+      return {
+        chart: null,
+        chartData: [0, 0, 0, 0, 0, 0, 0]
+      }
     },
-    height: {
-      type: String,
-      default: '283px'
-    }
-  },
-  data() {
-    return {
-      chart: null
-    }
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.initChart()
-    })
-  },
-  beforeDestroy() {
-    if (!this.chart) {
-      return
-    }
-    this.chart.dispose()
-    this.chart = null
-  },
-  methods: {
-    initChart() {
-      this.chart = echarts.init(this.$el, 'macarons')
+    beforeDestroy() {
+      if (!this.chart) {
+        return
+      }
+      this.chart.dispose()
+      this.chart = null
+    },
+    methods: {
+        sumSalesLatestWeek(){
+            sumSalesLatestWeek().then(response => {
+                const data = response.data;
+                data.forEach(item =>{
+                    const index = item.dayOfWeek;
+                    if (index ===1){
+                        this.chartData[6] = item.sum;
+                    }else{
+                        this.chartData[index-2] = item.sum;
+                    }
+                });
+                this.initChart();
+            }).catch(() => {
+            })
+        },
+        initChart() {
+          this.chart = echarts.init(this.$el, 'macarons')
 
-      this.chart.setOption({
-        title: {
-          text: '一周销售额',
-          x: 'right',
-          textStyle: {
-            fontSize: 20,
-            fontWeight: 'bolder',
-            color: '#f4516c'
-          }
-        },
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          top: 10,
-          left: '2%',
-          right: '2%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [{
-          type: 'category',
-          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-          axisTick: {
-            alignWithLabel: true
-          }
-        }],
-        yAxis: [{
-          type: 'value',
-          axisTick: {
-            show: false
-          }
-        }],
-        series: [{
-          name: '销售额',
-          type: 'bar',
-          color: '#f4516c',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: [79, 52, 200, 334, 390, 330, 220],
-          animationDuration
-        }]
-      })
-    }
-  }
+          this.chart.setOption({
+            title: {
+              text: '本周销售额',
+              x: 'right',
+              textStyle: {
+                fontSize: 20,
+                fontWeight: 'bolder',
+                color: '#f4516c'
+              }
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+              }
+            },
+            grid: {
+              top: 10,
+              left: '2%',
+              right: '2%',
+              bottom: '3%',
+              containLabel: true
+            },
+            xAxis: [{
+              type: 'category',
+              data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+              axisTick: {
+                alignWithLabel: true
+              }
+            }],
+            yAxis: [{
+              type: 'value',
+              minInterval: 1,
+              axisTick: {
+                show: false
+              }
+            }],
+            series: [{
+              name: '销售额',
+              type: 'bar',
+              color: '#f4516c',
+              stack: 'vistors',
+              barWidth: '60%',
+              data: this.chartData,
+              animationDuration
+            }]
+          })
+        }
+    },
+    mounted() {
+        this.sumSalesLatestWeek();
+        /*this.$nextTick(() => {
+            this.initChart()
+        })*/
+    },
 }
 </script>
